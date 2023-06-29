86099d049867d7228414497b721f6ac2a475329f
Neander de Souza <neander.faria@hotmail.com>
2023-06-29 14:50:02 -0300

    Publicação e manutenção - Publicando Storybook

--- packages/docs/.storybook/main.js
+++ packages/docs/.storybook/main.js
@@ -14,5 +14,12 @@ const config = {
   docs: {
     autodocs: "tag",
   },
+  viteFinal: (config, { configType }) => {
+    if (configType === "PRODUCTION") {
+      config.base = "/ignite-reactjs-05-design-system/";
+    }
+
+    return config;
+  },
 };
 export default config;
